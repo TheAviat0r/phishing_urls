b0f9209c3c29e000f0a49f2bbd42fbd10553604a
Dmitriy <parrot-studio@hotmail.com>
2017-11-29 00:17:46 +0300

    commit for merge

--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 from bcolors import bcolors
 from algo import BAD_SAMPLE_CONSTANT
 
+
 def get_response(answer, algo_type):
     response = bcolors.BOLD + str.ljust(answer[0].url, 40) + (' - [%s]' % algo_type)
 
@@ -9,7 +10,7 @@ def get_response(answer, algo_type):
     if answer[1] == 0:
         response += " --->" + bcolors.BOLD + bcolors.OKGREEN + " Not phishing" + bcolors.ENDC
     if answer[1] == 1:
-        response += " --->" + bcolors.BOLD + bcolors.FAIL + " Phishing" + bcolors.ENDC 
+        response += " --->" + bcolors.BOLD + bcolors.FAIL + " Phishing" + bcolors.ENDC
     if answer[1] == BAD_SAMPLE_CONSTANT:
         response += " --->" + bcolors.BOLD + bcolors.WARNING + " Bad sample" + bcolors.ENDC
 
