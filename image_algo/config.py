c4da4df1c2a2a409df0ec502fc71f42c72ed4305
Bulat Niatshin <niatshin@tarantool.org>
2018-02-11 23:48:59 +0300

    removed unused files for bot

--- image_algo/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-import global_config as cfg
-
-TARGET_IMAGE_DIR = os.path.join(cfg.SUPPLY_PATH, 'image_algo/target_screens/')
-PHISH_IMAGE_DIR = os.path.join(cfg.ALGOTMP, 'image_algo_tmp/phish_screens/')
-
-DEFAULT_HASH_SIZE = 8
--- neuro_algo/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-import global_config as cfg
-
-TARGET_NEURO_DIR = os.path.join(cfg.SUPPLY_PATH, 'neuro_algo/target_screens/')
-PHISH_NEURO_DIR = os.path.join(cfg.ALGOTMP, 'neuro_algo_tmp/phish_screens/')
